f653376fec7eb2ab596b2f7abd8b7da96f800aff
Biswajit Panday <biswajitmailid@gmail.com>
2024-12-29 17:24:17 +0600

    Fix Controller.

--- HyperCache/Controllers/CustomPropertiesController.cs
+++ HyperCache/Controllers/CustomPropertiesController.cs
@@ -1,19 +1,38 @@
-using HyperCache.Api.Models;
+using HyperCache.Api.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HyperCache.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class CustomPropertiesController : ControllerBase
+public class CustomPropertiesController(AppDbContext context) : ControllerBase
 {
-    public CustomPropertiesController()
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(string id)
     {
+        var customProperty = await context.CustomProperties
+                .FirstOrDefaultAsync(x => x.Id.ToString() == id);
+        return Ok(customProperty);
     }
 
     [HttpGet]
-    public ActionResult<CustomProperties> Get()
+    public async Task<IActionResult> Get()
     {
-        return Ok(new CustomProperties());
+        var customProperties = await context.CustomProperties
+            .ToListAsync();
+        return Ok(customProperties);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Search(string keyword)
+    {
+        if (string.IsNullOrEmpty(keyword))
+            return BadRequest("Keyword can't be empty!");
+
+        var customProperties = await context.CustomProperties
+            .Where(x => x.Name.Contains(keyword))
+            .ToListAsync();
+        return Ok(customProperties);
     }
 }
